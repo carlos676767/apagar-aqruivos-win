1e1ae4990b09dea6627b44aab100bc71bea3c3b2
carlos676767 <carlitobr730@gmail.com>
2024-05-23 15:41:07 -0700

    ok

--- script.cjs
+++ script.cjs
@@ -1,7 +1,7 @@
 const robot = require('robotjs');
 
-function moverMouseBarra() {
-    robot.moveMouse(1569, 17)
+function selecionarPosEclicar(valor1, valo2) {
+    robot.moveMouse(valor1, valo2)
     robot.mouseClick()
 }
 
@@ -9,9 +9,16 @@ function digitarTemp() {
     robot.typeString("%temp%")
     robot.keyTap("enter")
     robot.keyTap('a', ['control']);
+    robot.keyTap("delete")
+}
+
+const esperar = () => {
+
 }
 
 
-
-moverMouseBarra();
+selecionarPosEclicar(1569, 17);
 digitarTemp()
+esperar();
+selecionarPosEclicar(577, 376)
+selecionarPosEclicar(822, 411)
